77e3df1f250b6322a4e08866823cd38372b8e6dd
Guilherme5G <guiam2909@gmail.com>
2022-11-10 11:04:12 -0300

    Primeiro commit

--- script.js
+++ script.js
@@ -2,15 +2,37 @@ const ToDoField = document.querySelector('.to-do-field');
 const ToDoSend = document.querySelector('.to-do-send');
 const ToDoList = document.querySelector('.to-do-list')
 
-ToDoSend.addEventListener('click', function(e){
+function criali(){
     const li = document.createElement('li');
+    return li;
+}
+
+function criaButtonClear(){
     const buttonClear = document.createElement('button');
     buttonClear.setAttribute('class', 'button-clear');
     buttonClear.innerText = 'Deletar';
+    return buttonClear;
+}
+
+function criatarefa(li, buttonClear){
     li.innerText = ToDoField.value;
     li.appendChild(buttonClear);
     ToDoList.appendChild(li);
+}
+
+ToDoSend.addEventListener('click', function(e){
+    const li = criali();
+    const buttonClear = criaButtonClear();
+    criatarefa(li, buttonClear); 
 });
+
+ToDoField.addEventListener('keyup', function(e){
+    if(e.keyCode === 13){
+        const li = criali();
+        const buttonClear = criaButtonClear();
+        criatarefa(li, buttonClear); 
+    }
+})
 
 addEventListener('click', function(e){
     const el = e.target;
@@ -18,4 +40,4 @@ addEventListener('click', function(e){
     if(el.classList.contains('buttons-clear')){
         el.parentElement.remove();
     }
-})
+});
